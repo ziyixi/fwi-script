d6375f060f3f8f0ed5c8b856eae9567fd7089b4a
Ziyi Xi <xiziyi@msu.edu>
2019-07-27 15:02:57 -0400

    add debugger

---
+++ Script/asdf/scipts/find_missing_asdf.py
@@ -0,0 +1,22 @@
+from glob import glob
+from os.path import join, basename
+
+basedir = "/mnt/ls15/scratch/users/xiziyi/process_asdf/relocation/processed"
+
+
+def main():
+    allfiles = glob(join(basedir))
+    allfiles = [basename(item) for item in allfiles]
+
+    keys = set()
+    for item in allfiles:
+        splitter = item.split("_")
+        keys.add("_".join(splitter[:3]))
+    for item in keys:
+        keyfiles = glob(join(basedir, item+"*"))
+        if(len(keyfiles) != 9):
+            print(item)
+
+
+if __name__ == "__main__":
+    main()
