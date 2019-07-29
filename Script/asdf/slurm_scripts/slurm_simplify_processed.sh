2688a362c5818317b47babe3894407dddc9715ba
Ziyi Xi <xiziyi@msu.edu>
2019-07-29 11:19:46 -0400

    add slurm script to simplify

--- Script/asdf/slurm_scripts/slurm_simplify_processed.sh
+++ Script/asdf/slurm_scripts/slurm_simplify_processed.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=03:00:00             # limit of wall clock time - how long the job will run (same as -t)
 #SBATCH --ntasks=1                 # number of tasks - how many tasks (nodes) that you require (same as -n)
 #SBATCH --cpus-per-task=1           # number of CPUs (or cores) per task (same as -c)
-#SBATCH --mem-per-cpu=8G            # memory required per allocated CPU (or core) - amount of memory (in bytes)
+#SBATCH --mem-per-cpu=2G            # memory required per allocated CPU (or core) - amount of memory (in bytes)
 #SBATCH --job-name simplify      # you can give your job a name for easier identification (same as -J)
  
 ########## Command Lines to Run ##########
