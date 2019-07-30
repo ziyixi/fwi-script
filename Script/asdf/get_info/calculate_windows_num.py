5e0f611cab26a811a9d1eb45516d222801b2aed5
Ziyi Xi <xiziyi@msu.edu>
2019-07-29 23:39:23 -0400

    add calculate num windows

---
+++ Script/asdf/get_info/calculate_windows_num.py
@@ -0,0 +1,31 @@
+from glob import glob
+from os.path import join, basename
+import pickle
+
+basedir = "/mnt/ls15/scratch/users/xiziyi/process_asdf/windows_10s"
+output = "/mnt/ls15/scratch/users/xiziyi/process_asdf/logs/windows_num.txt"
+
+
+def load_pickle(fname):
+    with open(fname, "rb") as f:
+        data = pickle.load(fname)
+    return data
+
+
+def main():
+    allfiles = glob(join(basedir, "*pkl"))
+    with open(output, "w") as f:
+        for fpath in allfiles:
+            fname = basename(fpath)[:-4]
+            data = load_pickle(fpath)
+            num_windows = 0
+            len_windows = 0
+            for key in data:
+                for channel in data[key]:
+                    num_windows += len(channel)
+            num_windows = num_windows/len(data.keys())
+            f.write(f"{fname} {num_windows} \n")
+
+
+if __name__ == "__main__":
+    main()
