89623a70c38e1ee6a73e0c379f3f10c36f7f3655
Ziyi Xi <xiziyi@msu.edu>
2019-07-30 07:47:48 -0400

    generate psmeca

---
+++ Script/asdf/scipts/generate_psmeca_windows.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+raw = np.loadtxt("./psmeca_gcmts.25.log", dtype=str)
+windows = "/mnt/ls15/scratch/users/xiziyi/process_asdf/logs/windows_num.txt"
+new = np.loadtxt(windows, dtype=str)
+
+# for win in new:
+#     key=win[0].split(".")[0]
+#     for item in raw:
+
+for item in raw:
+    key = item[-1]
+    item[2] = 0
+    for win in new:
+        winkey = win[0].split(".")[0]
+        if(key == winkey):
+            item[2] = win[1]
+
+np.savetxt("./25d0.psmeca")
