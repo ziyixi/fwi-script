ef92ece24a6eb9e88916c05ff8e8b0c6a64c8db0
Ziyi Xi <xiziyi@msu.edu>
2019-07-31 10:42:56 -0400

    fix typo

--- Program/specfem_gll.jl/extract_region_from_existing/get_perturbation.jl
+++ Program/specfem_gll.jl/extract_region_from_existing/get_perturbation.jl
@@ -29,5 +29,5 @@ function main()
     mesh_info=sem_mesh_read(target_basedir,0)
     nspec=mesh_info.nspec
     # run generate_perturbation
-    generate_perturbation(target_basedir, reference_basedir, output_basedir, nproc, nspec:)
+    generate_perturbation(target_basedir, reference_basedir, output_basedir, nproc, nspec)
 end
