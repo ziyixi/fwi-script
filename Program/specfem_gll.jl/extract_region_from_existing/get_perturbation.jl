d6ad320f065057df286c2392c3a3d38f6916a1df
Ziyi Xi <xiziyi@msu.edu>
2019-07-31 11:32:45 -0400

    add parallel version to add auxi

--- Program/specfem_gll.jl/extract_region_from_existing/get_perturbation.jl
+++ Program/specfem_gll.jl/extract_region_from_existing/get_perturbation.jl
@@ -7,13 +7,13 @@ function parse_command_line()
 
     @add_arg_table s begin
         "--target_basedir"
-            help="the target gll directory"
+            help = "the target gll directory"
         "--reference_basedir"
-            help="the reference gll directory"
+            help = "the reference gll directory"
         "--output_basedir"
-            help="the output gll directory for perturbation"
+            help = "the output gll directory for perturbation"
         "--nproc"
-            help="number of processors the gll directory correspons to"
+            help = "number of processors the gll directory correspons to"
     end
     return parse_args(s)
 end
@@ -21,13 +21,14 @@ end
 function main()
     # parse args
     parsed_args = parse_command_line()
-    target_basedir=parsed_args["target_basedir"]
-    reference_basedir=parse_args["reference_basedir"]
-    output_basedir=parsed_args["output_basedir"]
-    nproc=parsed_args["nproc"]
+    @show parsed_args
+    target_basedir = parsed_args["target_basedir"]
+    reference_basedir = parse_args["reference_basedir"]
+    output_basedir = parsed_args["output_basedir"]
+    nproc = parsed_args["nproc"]
     # get nspec
-    mesh_info=sem_mesh_read(target_basedir,0)
-    nspec=mesh_info.nspec
+    mesh_info = sem_mesh_read(target_basedir, 0)
+    nspec = mesh_info.nspec
     # run generate_perturbation
     generate_perturbation(target_basedir, reference_basedir, output_basedir, nproc, nspec)
 end
