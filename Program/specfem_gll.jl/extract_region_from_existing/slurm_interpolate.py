d5cf30c71bc012e67b6cbce2c0422970bf6f83a7
Ziyi Xi <xiziyi@msu.edu>
2019-08-01 12:16:11 -0400

    disable remora

--- Program/specfem_gll.jl/extract_region_from_existing/slurm_interpolate.py
+++ Program/specfem_gll.jl/extract_region_from_existing/slurm_interpolate.py
@@ -10,7 +10,7 @@ new_model_dir = "/work/05880/tg851791/stampede2/japan_slab/gll_models/smaller_re
 model_tags = ",".join(["vph", "vpv", "vsh", "vsv", "eta", "qmu", "rho"])
 output_dir = "/work/05880/tg851791/stampede2/japan_slab/gll_models/smaller_region/perturbation/per_s362ani_tao"
 
-command1 = f"remora ibrun julia ./src/program/xsem_interp_mesh2.jl --nproc_old {nproc_old} --old_mesh_dir {old_mesh_dir} --old_model_dir {old_model_dir} --nproc_new {nproc_new} --new_mesh_dir {new_mesh_dir} --new_model_dir {new_model_dir} --model_tags {model_tags} --output_dir {output_dir}"
+command1 = f"ibrun julia ./src/program/xsem_interp_mesh2.jl --nproc_old {nproc_old} --old_mesh_dir {old_mesh_dir} --old_model_dir {old_model_dir} --nproc_new {nproc_new} --new_mesh_dir {new_mesh_dir} --new_model_dir {new_model_dir} --model_tags {model_tags} --output_dir {output_dir}"
 
 s = Slurm("interp", {"partition": "skx-normal",
                      "nodes": 14, "ntasks": 324, "time": "02:00:00", "account": "TG-EAR140030"})
