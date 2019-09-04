16cf159dd85e9293acccb4931075e26ca34e538d
Ziyi Xi <xiziyi@msu.edu>
2019-09-04 10:15:04 -0400

    debug

--- Script/asdf/depth_relocation/find_problem_json.py
+++ Script/asdf/depth_relocation/find_problem_json.py
@@ -6,8 +6,8 @@ import tqdm
 basedir = "/scratch/05880/tg851791/relocation/work/misfit_json"
 allfiles = glob(join(basedir, "*json"))
 for item in tqdm.tqdm(allfiles):
-    # try:
-    f = open(item, "r")
-    data = json.laod(f)
-    # except:
-    # print(basename(item))
+    try:
+        f = open(item, "r")
+        data = json.load(f)
+    except:
+        print(basename(item))
