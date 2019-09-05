c132f2572434b32aaefe956366ca61b877c3d50a
Ziyi Xi <xiziyi@msu.edu>
2019-09-05 09:59:39 -0400

    use knl node to calculate snr

--- Script/asdf/depth_relocation/submit_snr_job_mpi.sh
+++ Script/asdf/depth_relocation/submit_snr_job_mpi.sh
@@ -26,9 +26,9 @@
 #SBATCH -o snr.o%j       # Name of stdout output file
 #SBATCH -e snr.e%j       # Name of stderr error file
 #SBATCH -p normal      # Queue (partition) name
-#SBATCH -N 10               # Total # of nodes (must be 1 for serial)
+#SBATCH -N 5               # Total # of nodes (must be 1 for serial)
 #SBATCH -n 68               # Total # of mpi tasks (should be 1 for serial)
-#SBATCH -t 02:00:00        # Run time (hh:mm:ss)
+#SBATCH -t 01:00:00        # Run time (hh:mm:ss)
 #SBATCH -A TG-EAR140030       # Allocation name (req'd if you have more than 1)
 
 # Other commands must follow all #SBATCH directives...
