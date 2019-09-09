9f4a9674aac0eaa46198169fe5912a6903c04878
Ziyi Xi <xiziyi@msu.edu>
2019-09-09 15:22:39 -0400

    add a script to extract amp info

---
+++ Script/asdf/depth_relocation/amp_related/cal_amp_ratio.py
@@ -0,0 +1,62 @@
+"""
+Calculate amp ratios using the window json files.
+"""
+import numpy as np
+from glob import glob
+from os.path import join, basename
+import json
+import pandas as pd
+
+basedir = "/scratch/05880/tg851791/relocation/work/misfit_json"
+process_flag = "d0.preprocessed_10s_to_120s.surf.json"
+
+
+def get_keys():
+    all_files = glob(join(basedir, f"*{process_flag}"))
+    result = set()
+    for file_path in all_files:
+        fname = basename(file_path)
+        gcmtid = fname.split(".")[0]
+        result.add(gcmtid)
+    return sorted(result)
+
+
+def extract_amp_info(gcmtid_list):
+    result = {}
+    for gcmtid in gcmtid_list:
+        result[gcmtid] = {
+            "pz_sz": [],
+            "pr_sr": [],
+            "pz_pr": [],
+            "sz_sr": [],
+            "sz_st": [],
+            "sr_st": []
+        }
+        file_path = join(basedir, f"{gcmtid}.{process_flag}")
+        with open(file_path, "r") as f:
+            data_dict = json.load(f)
+            for net_sta in data_dict:
+                amp_info = data_dict[net_sta]["amplitude"]
+                pz = (amp_info["z"]["p"] or amp_info["z"]["pn"])
+                sz = (amp_info["z"]["s"])
+                pr = (amp_info["r"]["p"] or amp_info["r"]["pn"])
+                sr = (amp_info["r"]["s"])
+                st = (amp_info["t"]["s"])
+                if(pz and sz):
+                    result[gcmtid]["pz_sz"].append(pz/sz)
+                if(pr and sr):
+                    result[gcmtid]["pr_sr"].append(pr/sr)
+                if(pz and pr):
+                    result[gcmtid]["pz_pr"].append(pz/pr)
+                if(sz and sr):
+                    result[gcmtid]["sz_sr"].append(sz/sr)
+                if(sz and st):
+                    result[gcmtid]["sz_st"].append(sz/st)
+                if(sr and st):
+                    result[gcmtid]["sr_st"].append(sr/st)
+    json.dump(result, open("amp_ratio.json", 'w'))
+
+
+if __name__ == "__main__":
+    gcmtid_list = get_keys()
+    extract_amp_info(gcmtid_list)
