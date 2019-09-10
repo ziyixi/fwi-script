a87d48ea5261a7196bf909f80f0ad351d9ef34f5
Ziyi Xi <xiziyi@msu.edu>
2019-09-10 09:47:51 -0400

    fix typo

--- Script/smaller_region_related/copy_cmtsolution.py
+++ Script/smaller_region_related/copy_cmtsolution.py
@@ -10,7 +10,7 @@ basedir = "/mnt/research/seismolab2/japan_slab/cmts/Japan_slab"
 target = "/mnt/research/seismolab2/japan_slab/cmts/small_region"
 
 # get all gcmt ids
-psmeca_data = np.loadtxt("./psmeca_gcmts_Japan_Slab")
+psmeca_data = np.loadtxt("./psmeca_gcmts_Japan_Slab", dtype=np.str)
 gcmt_ids = set(psmeca_data[:, -1])
 for item in sorted(gcmt_ids):
     command = f"cp {join(basedir,item)} {join(target,item)}"
