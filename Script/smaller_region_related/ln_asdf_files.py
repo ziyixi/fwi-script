96b68fc5093380601e52c62950a946e6db712328
Ziyi Xi <xiziyi@msu.edu>
2019-09-18 17:40:52 -0400

    add script to ln asdf files to the small region

--- Script/smaller_region_related/ln_asdf_files.py
+++ Script/smaller_region_related/ln_asdf_files.py
@@ -1,5 +1,5 @@
 import subprocess
-from os.path import join, basename
+from os.path import join, basename, isfile
 import numpy as np
 
 psmeca_data = np.loadtxt("./psmeca_gcmts_Japan_Slab", dtype=np.str)
@@ -12,13 +12,10 @@ target_dir = "/scratch/05880/tg851791/process_data/asdf_raw_all_smallregion"
 for gcmtid in sorted(gcmtid_set):
     # try to ln from dir1
     command = f"ln -s ../{source_dir1}/raw_{gcmtid}.h5 {target_dir}"
-    try:
+    if(isfile(f"../{source_dir1}/raw_{gcmtid}.h5")):
         subprocess.call(command, shell=True)
-    except:
-        pass
+
     # try to ln from dir2
     command = f"ln -s ../{source_dir2}/raw_{gcmtid}.h5 {target_dir}"
-    try:
+    if(isfile(f"../{source_dir2}/raw_{gcmtid}.h5")):
         subprocess.call(command, shell=True)
-    except:
-        pass
