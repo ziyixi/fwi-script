b59fc01192ff4eb8038a9a6bf221aa0fab7ff6dd
Ziyi Xi <xiziyi@msu.edu>
2019-09-20 13:30:17 -0400

    check which one is abnormal sacpz

---
+++ Script/smaller_region_related/check_sacpz_type.py
@@ -0,0 +1,13 @@
+from glob import glob
+from os.path import join, basename
+
+basedir = "/scratch/05880/tg851791/process_data/sac_raw_all_smallregion"
+
+for fpath in sorted(glob(join(basedir, "*"))):
+    sacpzpath = join(fpath, "SACPZ", "*")
+    value = False
+    for item in sacpzpath:
+        if("BHZ" in item):
+            value = True
+    if(value):
+        print(basename(fpath))
