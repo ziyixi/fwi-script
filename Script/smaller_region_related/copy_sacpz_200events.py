7a001d47910b47186dd20ad41f6d19f5978b6d8e
Ziyi Xi <xiziyi@msu.edu>
2019-09-20 13:11:06 -0400

    update for broken

--- Script/smaller_region_related/copy_sacpz_200events.py
+++ Script/smaller_region_related/copy_sacpz_200events.py
@@ -4,6 +4,8 @@ copy sacpz files for 200 events.
 from os.path import join, basename
 from glob import glob
 import subprocess
+import multiprocessing
+import tqdm
 
 output_dir = "/scratch/05880/tg851791/process_data/sac_raw_all_smallregion"
 source_dir = "/scratch/05880/tg851791/pazs_EARA/pazs_EARA2014"
@@ -13,6 +15,12 @@ all_paths = glob(join(source_dir, "*"))
 all_gcmtids = [basename(item) for item in all_paths]
 
 # copy files for each gcmtid
-for gcmtid in all_gcmtids:
+
+
+def kernel(gcmtid):
     command = f"cp {source_dir}/{gcmtid}/* {output_dir}/{gcmtid}/SACPZ"
     subprocess.call(command, shell=True)
+
+
+with multiprocessing.Pool(processes=48) as pool:
+    r = list(tqdm.tqdm(pool.imap(kernel, all_gcmtids), total=len(all_gcmtids)))
