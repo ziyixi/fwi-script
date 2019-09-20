52d782b3739840f083c0096d4c29287d0f07a160
Ziyi Xi <xiziyi@msu.edu>
2019-09-20 16:59:34 -0400

    debug

--- Script/smaller_region_related/check_sacpz_type.py
+++ Script/smaller_region_related/check_sacpz_type.py
@@ -5,7 +5,7 @@ basedir = "/scratch/05880/tg851791/process_data/sac_raw_all_smallregion"
 
 for fpath in sorted(glob(join(basedir, "*"))):
     sacpzpath = join(fpath, "SACPZ", "*")
-    print(len(sacpzpath))
+    print(sacpzpath)
     value = False
     for item in sacpzpath:
         if("BHZ" in item):
