cd799fc8dd6c258355641ad741b0aba508d69732
Ziyi Xi <xiziyi@msu.edu>
2019-09-21 13:32:35 -0400

    add a script to extract sacpz files for fnet

--- Script/smaller_region_related/extract_fnet_sacpz.py
+++ Script/smaller_region_related/extract_fnet_sacpz.py
@@ -9,6 +9,7 @@ output_dir = "/mnt/home/xiziyi/data/fnet_sacpz"
 
 # get gcmtid to extract
 gcmtids = np.loadtxt("./from284.txt", dtype=np.str)
+station_keys = {}
 
 # extract all sacpzs to temp_dir
 for gcmtid in gcmtids:
@@ -16,11 +17,6 @@ for gcmtid in gcmtids:
     subprocess.call(command, shell=True)
     command = f"rdseed -pf {basedir}/{gcmtid}/{gcmtid}.SEED -q {temp_dir}/{gcmtid}"
     subprocess.call(command, shell=True)
-
-# * rename and combine sacpzs
-# get keys
-station_keys = {}
-for gcmtid in gcmtids:
     station_keys[gcmtid] = set()
     all_files = glob(join(temp_dir, gcmtid, "SAC_PZs_*"))
     all_files = [basename(item) for item in all_files]
@@ -38,6 +34,27 @@ for gcmtid in gcmtids:
             for f in sta_files:
                 with open(f, "w") as infile:
                     outfile.write(infile.read())
+
+# # * rename and combine sacpzs
+# # get keys
+# for gcmtid in gcmtids:
+#     station_keys[gcmtid] = set()
+#     all_files = glob(join(temp_dir, gcmtid, "SAC_PZs_*"))
+#     all_files = [basename(item) for item in all_files]
+#     for fname in all_files:
+#         fname_spliter = fname.split("_")
+#         sta = fname_spliter[3]
+#         station_keys[gcmtid].add(sta)
+#     # mkdir
+#     command = f"mkdir -p {output_dir}/{gcmtid}"
+#     subprocess.call(command, shell=True)
+#     # combine files
+#     for sta in sorted(station_keys[gcmtid]):
+#         sta_files = glob(join(temp_dir, gcmtid, f"SAC_PZs_BO_{sta}_*"))
+#         with open(f"{output_dir}/{gcmtid}/BO.{sta}", "w") as outfile:
+#             for f in sta_files:
+#                 with open(f, "w") as infile:
+#                     outfile.write(infile.read())
 
 # # combine files and output to output_dir
 # for gcmtid in gcmtids:
