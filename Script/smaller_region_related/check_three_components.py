09a9557e3e705a180187fe3a1d557109cdc94c17
Ziyi Xi <xiziyi@msu.edu>
2019-09-21 14:28:20 -0400

    add script to check three comp

--- Script/smaller_region_related/check_three_components.py
+++ Script/smaller_region_related/check_three_components.py
@@ -1,18 +1,33 @@
 from glob import glob
 from os.path import join, basename
+import subprocess
 
 base_dir = "/scratch/05880/tg851791/process_data/sac_raw_all_smallregion"
 
 all_dirs = glob(join(base_dir, "*"))
 for each_dir in all_dirs:
     gcmtid = basename(each_dir)
+
+    command = f"mkdir -p {base_dir}/{gcmtid}/extra"
+    subprocess.call(command, shell=True)
+    command = f"mv {base_dir}/{gcmtid}/SACPZ {base_dir}/{gcmtid}/PZ"
+    subprocess.call(command, shell=True)
+
     sac_files = sorted(glob(join(each_dir, "*")))
     sac_files = [basename(item) for item in sac_files]
     sac_files.remove("SACPZ")
     no3comp_num = 0
+    thekey = set()
     for sac_file in sac_files:
         net, sta, comp = sac_file.split(".")
-        comp3_files = glob(join(each_dir, f"{net}.{sta}*"))
+        thekey.add(".".join([net, sta]))
+        # comp3_files = glob(join(each_dir, f"{net}.{sta}*"))
+        # if(len(comp3_files) % 3 != 0):
+        #     no3comp_num += 1
+    for net_sta in sorted(thekey):
+        comp3_files = glob(join(each_dir, f"{net_sta}*"))
         if(len(comp3_files) % 3 != 0):
             no3comp_num += 1
+            command = f"mv {each_dir}/{net_sta}* {each_dir}/extra"
+            subprocess.call(command, shell=True)
     print(gcmtid, no3comp_num)
