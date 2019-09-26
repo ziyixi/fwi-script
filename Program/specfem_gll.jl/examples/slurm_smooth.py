ec51a9dc1bcc9715662a4cf80aaf754d177e2920
Ziyi Xi <xiziyi@msu.edu>
2019-09-26 12:45:42 -0400

    add slurm interp

--- Program/specfem_gll.jl/examples/slurm_smooth.py
+++ Program/specfem_gll.jl/examples/slurm_smooth.py
@@ -13,4 +13,5 @@ output_dir = "/scratch/05880/tg851791/work/generate_hybrid_v703/gll_work/perturb
 out_suffix = ".bin"
 sem_url = "/work/05880/tg851791/stampede2/fwi-script/Program/specfem_gll.jl/examples/sem_utils/bin"
 
-command = f"module load netcdf; date; cd {sem_url}; ibrun ./xsem_smooth {nproc} {mesh_dir} {model_dir} {model_tags} {sigma_h} {sigma_v} {output_dir} {out_suffix}; date;"
+command = f"module load netcdf/4.6.2; date; cd {sem_url}; ibrun ./xsem_smooth {nproc} {mesh_dir} {model_dir} {model_tags} {sigma_h} {sigma_v} {output_dir} {out_suffix}; date;"
+s.run(command)
